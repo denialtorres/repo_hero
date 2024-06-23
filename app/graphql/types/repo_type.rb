adac9b67d95dd7abd1cb1d97e434aead306e1b99
denialtorres <denial.torres@gmail.com>
2024-06-23 11:53:55 -0600

    Queries for finding categorized repos and to find repos categoris

--- app/graphql/types/repo_type.rb
+++ app/graphql/types/repo_type.rb
@@ -2,7 +2,8 @@ module Types
   class RepoType < Types::BaseObject
     field :name, String, null: false
     field :url, String, null: false
-    field :name_reversed, String, null: false
+    field :name_reversed, String, null: false, complexity: 10
+    field :categories, [CategoryType], null: false
 
     def name_reversed
       object.name.reverse
