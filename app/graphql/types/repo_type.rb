1f918db36ba8502e219d4dd4af85cd2dd8abf919
denialtorres <denial.torres@gmail.com>
2024-07-07 11:50:16 -0600

    Union types class

--- app/graphql/types/repo_type.rb
+++ app/graphql/types/repo_type.rb
@@ -5,6 +5,9 @@ module Types
     field :name_reversed, String, null: false, complexity: 10
     field :categories, [CategoryType], null: false
     field :reviews, ReviewType.connection_type, null: false, default_page_size: 10, max_page_size: 20
+    # To find the relevant activities for a repo, this GraphQL field will use the
+    # Repo#activities method defined by the has_many association on the Repo model.
+    field :activities, ActivityType.connection_type, null: false, default_page_size: 10, max_page_size: 20, complexity: 10
 
     def name_reversed
       object.name.reverse
