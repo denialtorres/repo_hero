1010167d038a897e072083ea245cb752e6114676
denialtorres <denial.torres@gmail.com>
2024-07-08 14:26:15 -0600

    Add delete mutation

---
+++ app/graphql/mutations/reviews/delete.rb
@@ -0,0 +1,13 @@
+module Mutations
+  module Reviews
+    class Delete < BaseMutation
+      type Types::ReviewType
+
+      argument :review_id, ID, required: true
+
+      def resolve(review_id: )
+        Review.find(review_id).destroy
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
   class MutationType < Types::BaseObject
     field :add_review, mutation: Mutations::Reviews::Add
     field :update_review, mutation: Mutations::Reviews::Update
+    field :delete_review, mutation: Mutations::Reviews::Delete
   end
 end
